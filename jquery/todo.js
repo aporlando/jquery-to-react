a26475dd7d2d90b8024286abd6a7663c575b90fc
Ali Orlando <aorlando@onshift.com>
2017-12-29 17:21:11 -0500

    complete & delete todos

--- jquery/todo.js
+++ jquery/todo.js
@@ -1,17 +1,34 @@
+var uniqueCounter = 0;
 function addTodo(todoText, completed) {
+    uniqueCounter = uniqueCounter+1;
+    var todoId = 'todo' + uniqueCounter;
     $('#todos').append(
-        '<li>' +
-        todoText +
+        '<li class="list-group-item" data-item="' + todoId + '">' +
+        '  <div class="form-check" onchange="completeTodo(this)">' +
+        '    <input type="checkbox" class="form-check-input" id="' + todoId + '">' +
+        '    <label class="form-check-label" for="' + todoId + '">' +
+        (completed ? '<del>' + todoText + '</del>' : todoText) +
+        '    </label>' +
+        '  </div>' +
+        '  <button onclick="deleteTodo(this)">Delete</button>' +
         '</li>'
     );
 }
 
-function deleteTodo() {
-
+function deleteTodo(todo) {
+    console.log('complete item', todo);
+    $(todo).parent().remove();
 }
 
-function completeTodo() {
-
+function completeTodo(todo) {
+    var label = $(todo).find('label');
+    if ($(todo).find('input').prop('checked')) {
+        label.html('<del>' +
+            label.text() +
+            '</del>');
+    } else {
+        label.html(label.text());
+    }
 }
 
 $("#addForm").on('submit', function(e) {
